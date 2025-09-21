9742217e26f86a39bbae8424842e0a52000fedbf
Soroush Eghdami <soroush.egh@gmail.com>
2025-09-22 00:15:33 +0330

    Fixing the Templates(modiffied the css) for the product list and cart

--- ecom/urls.py
+++ ecom/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     # finally include shop at root — product_list will handle `/`
     path('', include(('shop.urls', 'shop'), namespace='shop')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
