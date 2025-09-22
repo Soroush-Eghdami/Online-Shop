9d31a45e6807d5c8b4817459be6ec0393d704ef1
Soroush Eghdami <soroush.egh@gmail.com>
2025-09-22 15:58:16 +0330

    fixing cart view and html

--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
 
+@require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    cart.add(product=product, quantity=1)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product,
+            quantity=cd['quantity'],
+            override_quantity=cd['override']
+        )
     return redirect('cart:cart_detail')
+
 
 def cart_remove(request, product_id):
     cart = Cart(request)
@@ -16,6 +26,14 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
+
+    # prepare update forms for each cart item
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'], 'override': True}
+        )
+
     return render(request, 'cart/detail.html', {'cart': cart})
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from cart.forms import CartAddProductForm
 
 def product_list(request):
-    products = Product.objects.filter(available=True)
+    products = Product.objects.all()
     return render(request, 'shop/product_list.html', {'products': products})
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, available=True)
-    return render(request, 'shop/product_detail.html', {'product': product})
+    product = get_object_or_404(Product, slug=slug)
+    cart_product_form = CartAddProductForm()
+    return render(
+        request,
+        'shop/product_detail.html',
+        {'product': product, 'cart_product_form': cart_product_form}
+    )
