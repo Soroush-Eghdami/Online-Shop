11b4faf9922677976d45a773bb02af840ef1cf54
Soroush Eghdami <soroush.egh@gmail.com>
2025-09-24 11:28:34 +0330

    trying to fix cart

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1, initial=1)
+    quantity = forms.IntegerField(
+        min_value=1, max_value=20,
+        widget=forms.NumberInput(attrs={'class': 'quantity-input'})
+    )
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
